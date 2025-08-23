120d33466c323c0c4cc54b3736233f97e2418bc3
JiriStipek <567776@muni.cz>
2025-08-23 18:50:39 +0200

    feat: add model binding + validation

--- src/Order.Model/OrderDetail.cs
+++ src/Order.Model/OrderDetail.cs
@@ -1,26 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Order.Model
 {
     public class OrderDetail
     {
+        [Required]
         public Guid Id { get; set; }
-        
+
+        [Required]
         public Guid ResellerId { get; set; }
-        
+
+        [Required]
         public Guid CustomerId { get; set; }
-        
+
+        [Required]
         public Guid StatusId { get; set; }
 
+        [Required, StringLength(100)]
         public string StatusName { get; set; }
 
+        [Required]
         public DateTime CreatedDate { get; set; }
 
+        [Range(0, double.MaxValue)]
         public decimal TotalCost { get; set; }
 
+        [Range(0, double.MaxValue)]
         public decimal TotalPrice { get; set; }
 
+        [MinLength(1)]
         public IEnumerable<OrderItem> Items { get; set; }
 
     }
--- src/Order.Model/OrderItem.cs
+++ src/Order.Model/OrderItem.cs
@@ -1,29 +1,41 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Order.Model
 {
     public class OrderItem
     {
+        [Required]
         public Guid Id { get; set; }
 
+        [Required]
         public Guid OrderId { get; set; }
-        
+
+        [Required]
         public Guid ServiceId { get; set; }
-        
+
+        [Required, StringLength(100)]
         public string ServiceName { get; set; }
 
+        [Required]
         public Guid ProductId { get; set; }
 
+        [Required, StringLength(100)]
         public string ProductName { get; set; }
-        
+
+        [Range(1, int.MaxValue)]
         public int Quantity { get; set; }
 
+        [Range(0, double.MaxValue)]
         public decimal UnitCost { get; set; }
 
+        [Range(0, double.MaxValue)]
         public decimal UnitPrice { get; set; }
 
+        [Range(0, double.MaxValue)]
         public decimal TotalCost { get; set; }
 
+        [Range(0, double.MaxValue)]
         public decimal TotalPrice { get; set; }
     }
 }
